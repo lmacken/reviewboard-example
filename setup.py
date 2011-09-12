14af7cdbf4a055e26e2683facf472e9c2646b95b
Luke Macken <lmacken@redhat.com>
2011-09-12 13:37:45 -0400

    Remove the explicit dependency on 1.5.6

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['ReviewBoard==1.5.6'],
+      install_requires=['ReviewBoard'],
      )
